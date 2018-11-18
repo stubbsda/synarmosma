fb773f9f92542f864dc999998a0402c81b5daeff
Daniel Stubbs <dnl.stubbs@gmail.com>
2018-11-18 16:37:06 -0500

    I have continued to work on improving the Matrix class and documenting it with Doxygen; all the remains to document are the various friend functions.

--- matrix.cpp
+++ matrix.cpp
@@ -83,7 +83,7 @@ void Matrix<kind>::display(std::ostream& s) const
 
   for(i=0; i<nrow; ++i) {
     for(j=0; j<ncolumn; ++j) {
-      w = zero;
+      w = Matrix<kind>::zero;
       found = false;
       for(k=0; k<elements[i].size(); ++k) {
         if (elements[i][k].second == j) {
@@ -107,16 +107,14 @@ void Matrix<kind>::display(std::ostream& s) const
 template<class kind>
 void Matrix<kind>::convert(kind* A,char mtype) const
 {
-#ifdef DEBUG
-  assert(mtype == 'r' || mtype == 'c');
-#endif
+  if (mtype != 'r' && mtype != 'c') throw std::invalid_argument("The conversion type for the matrix must be row-oriented or column-oriented!");
   // This method supposes that A has already been allocated the appropriate amount
   // of memory...
   unsigned int i,j;
 
   for(i=0; i<nrow; ++i) {
     for(j=0; j<ncolumn; ++j) {
-      A[nrow*i+j] = zero;
+      A[nrow*i+j] = Matrix<kind>::zero;
     }
   }
   if (mtype == 'r') {
@@ -138,19 +136,57 @@ void Matrix<kind>::convert(kind* A,char mtype) const
 }
 
 namespace SYNARMOSMA {
-  // A divisibility test is meaningless in the context of fields like \mathbb{R} and \mathbb{C}...
+  // A divisibility test is largely meaningless in the context of fields like \mathbb{R} and \mathbb{C}...
   template<>
   bool Matrix<double>::divisible(unsigned int n,unsigned int* out1,unsigned int* out2,double* f) const
   {
     if (n >= nrow) throw std::invalid_argument("The row number argument is illegal for this matrix!");
-    return true;
+    unsigned int i,j;
+    double b1,b2 = 0.0;
+
+    for(i=0; i<elements[n].size(); ++i) {
+      if (elements[n][i].second == n) b2 = elements[n][i].first;
+    }
+    if (std::abs(b2) < std::numeric_limits<double>::epsilon()) return false;
+    for(i=n+1; i<nrow; ++i) {
+      for(j=0; j<elements[i].size(); ++j) {
+        if (elements[i][j].second <= n) continue;
+        b1 = elements[i][j].first;
+        if (std::abs(b1) > std::numeric_limits<double>::epsilon()) {
+          *out1 = i;
+          *out2 = elements[i][j].second;
+          *f = b1/b2;
+          return true;
+        }
+      }
+    }
+    return false;
   }
 
   template<>
   bool Matrix<std::complex<double> >::divisible(unsigned int n,unsigned int* out1,unsigned int* out2,std::complex<double>* f) const
   {
     if (n >= nrow) throw std::invalid_argument("The row number argument is illegal for this matrix!");
-    return true;
+    unsigned int i,j;
+    std::complex<double> b1,b2 = 0.0;
+
+    for(i=0; i<elements[n].size(); ++i) {
+      if (elements[n][i].second == n) b2 = elements[n][i].first;
+    }
+    if (std::abs(b2) < std::numeric_limits<double>::epsilon()) return false;
+    for(i=n+1; i<nrow; ++i) {
+      for(j=0; j<elements[i].size(); ++j) {
+        if (elements[i][j].second <= n) continue;
+        b1 = elements[i][j].first;
+        if (std::abs(b1) > std::numeric_limits<double>::epsilon()) {
+          *out1 = i;
+          *out2 = elements[i][j].second;
+          *f = b1/b2;
+          return true;
+        }
+      }
+    }
+    return false;
   }
 }
 
@@ -158,16 +194,16 @@ template<class kind>
 bool Matrix<kind>::divisible(unsigned int n,unsigned int* out1,unsigned int* out2,kind* f) const
 {
   if (n >= nrow) throw std::invalid_argument("The row number argument is illegal for this matrix!");
-  unsigned int i,j,nu = n;
+  unsigned int i,j;
   kind b1,b2 = Matrix<kind>::zero;
 
   for(i=0; i<elements[n].size(); ++i) {
-    if (elements[n][i].second == nu) b2 = elements[n][i].first;
+    if (elements[n][i].second == n) b2 = elements[n][i].first;
   }
   if (b2 == Matrix<kind>::zero) return false;
   for(i=n+1; i<nrow; ++i) {
     for(j=0; j<elements[i].size(); ++j) {
-      if (elements[i][j].second <= nu) continue;
+      if (elements[i][j].second <= n) continue;
       b1 = elements[i][j].first;
       if ((b1 % b2) != 0) {
         *out1 = i;
@@ -184,9 +220,7 @@ template<class kind>
 void Matrix<kind>::homotopy_scaling(kind a1,kind a2,Matrix<kind>* output) const
 {
   // A method to compute output = a1*A + a2*I...
-#ifdef DEBUG
-  assert(output->nrow == nrow && output->ncolumn == ncolumn);
-#endif
+  if (output->nrow != nrow || output->ncolumn != ncolumn) std::invalid_argument("The output matrix matrix dimensions are illegal!");
   unsigned int i,j;
   bool diagonal;
 
@@ -249,12 +283,12 @@ void Matrix<kind>::multiply(const std::vector<kind>& b,std::vector<kind>& output
 template<class kind>
 kind Matrix<kind>::determinant() const
 {
-  assert(nrow == ncolumn);
+  if (nrow != ncolumn) throw std::runtime_error("The determinant can only be computed for square matrices!");
   unsigned int i,j,k,q;
   bool found;
   std::vector<int> symbols,rest;
   std::vector<std::vector<int> > plist;
-  kind product,output = zero;
+  kind product,output = Matrix<kind>::zero;
 
   for(i=0; i<nrow; ++i) {
     rest.push_back(i);
@@ -275,7 +309,7 @@ kind Matrix<kind>::determinant() const
         }
       }
       if (!found) {
-        product = zero;
+        product = Matrix<kind>::zero;
         break;
       }
     }
@@ -478,7 +512,7 @@ void Matrix<kind>::get_diagonal(std::vector<kind>& output) const
   output.clear();
 
   for(i=0; i<nrow; ++i) {
-    d = zero;
+    d = Matrix<kind>::zero;
     for(j=0; j<elements[i].size(); ++j) {
       if (elements[i][j].second == i) {
         d = elements[i][j].first;
@@ -508,10 +542,7 @@ namespace SYNARMOSMA {
 template<class kind>
 void Matrix<kind>::gauss_seidel_solver(std::vector<kind>& soln,const std::vector<kind>& source,double threshold,int max_its)
 {
-#ifdef DEBUG
-  assert(soln.size() == nrow);
-  assert(source.size() == nrow);
-#endif
+  if (soln.size() != nrow || source.size() != nrow) throw std::invalid_argument("The vector dimensions are wrong for the Gauss-Seidel solver!");
   unsigned int i,j;
   int its = 0;
   kind sum;
@@ -520,7 +551,7 @@ void Matrix<kind>::gauss_seidel_solver(std::vector<kind>& soln,const std::vector
   std::vector<kind> x_new,diagonal,x = soln,b = source;
 
   for(i=0; i<nrow; ++i) {
-    x_new.push_back(zero);
+    x_new.push_back(Matrix<kind>::zero);
   }
 
   if (!diagonally_dominant(true)) {
@@ -543,7 +574,7 @@ void Matrix<kind>::gauss_seidel_solver(std::vector<kind>& soln,const std::vector
 
   error = 0.0;
   for(i=0; i<nrow; ++i) {
-    sum = zero;
+    sum = Matrix<kind>::zero;
     for(j=0; j<elements[i].size(); ++j) {
       sum += elements[i][j].first*x[elements[i][j].second];
     }
@@ -562,7 +593,7 @@ void Matrix<kind>::gauss_seidel_solver(std::vector<kind>& soln,const std::vector
 #pragma omp parallel for default(shared) private(i,j,sum)
 #endif
     for(i=0; i<nrow; ++i) {
-      sum = zero;
+      sum = Matrix<kind>::zero;
       for(j=0; j<elements[i].size(); ++j) {
         if (elements[i][j].second == i) continue;
         sum += elements[i][j].first*x[elements[i][j].second];
@@ -571,7 +602,7 @@ void Matrix<kind>::gauss_seidel_solver(std::vector<kind>& soln,const std::vector
     }
     error_new = 0.0;
     for(i=0; i<nrow; ++i) {
-      sum = zero;
+      sum = Matrix<kind>::zero;
       for(j=0; j<elements[i].size(); ++j) {
         sum += elements[i][j].first*x_new[elements[i][j].second];
       }
@@ -680,6 +711,7 @@ int Matrix<kind>::serialize(std::ofstream& s) const
 
   s.write((char*)(&nrow),sizeof(int)); count += sizeof(int);
   s.write((char*)(&ncolumn),sizeof(int)); count += sizeof(int);
+  s.write((char*)(&normalized),sizeof(bool)); count += sizeof(bool);
   count += write_elements(s);
 
   return count;
@@ -720,6 +752,22 @@ int Matrix<kind>::read_elements(std::ifstream& s)
       elements[i].push_back(std::pair<kind,unsigned int>(x,k));
     }
   }
+  return count;
+}
+
+template<class kind>
+int Matrix<kind>::deserialize(std::ifstream& s)
+{
+  int count = 0;
+
+  clear();
+
+  s.read((char*)(&nrow),sizeof(int)); count += sizeof(int);
+  s.read((char*)(&ncolumn),sizeof(int)); count += sizeof(int);
+  s.read((char*)(&normalized),sizeof(bool)); count += sizeof(bool);
+  elements = new std::vector<std::pair<kind,unsigned int> >[nrow];
+  count += read_elements(s);
+
   return count;
 }
 
@@ -778,9 +826,9 @@ template<class kind>
 kind Matrix<kind>::get_first_nonzero(unsigned int r) const
 {
   if (r >= nrow) throw std::invalid_argument("The row number argument is illegal for this matrix!");
-  if (elements[r].empty()) return zero;
+  if (elements[r].empty()) return Matrix<kind>::zero;
   unsigned int i,min_index = ncolumn;
-  kind output = zero;
+  kind output = Matrix<kind>::zero;
 
   for(i=0; i<elements[r].size(); ++i) {
     if (elements[r][i].second < min_index && std::abs(elements[r][i].first) > std::numeric_limits<double>::epsilon()) {
@@ -821,21 +869,6 @@ bool Matrix<kind>::empty_row(unsigned int r) const
 }
 
 template<class kind>
-int Matrix<kind>::deserialize(std::ifstream& s)
-{
-  int count = 0;
-
-  clear();
-
-  s.read((char*)(&nrow),sizeof(int)); count += sizeof(int);
-  s.read((char*)(&ncolumn),sizeof(int)); count += sizeof(int);
-  elements = new std::vector<std::pair<kind,unsigned int> >[nrow];
-  count += read_elements(s);
-
-  return count;
-}
-
-template<class kind>
 void Matrix<kind>::initialize(unsigned int n,unsigned int m)
 {
   if (n == 0 || m == 0) throw std::invalid_argument("The matrix dimensions must be greater than zero!");
@@ -872,9 +905,7 @@ void Matrix<kind>::transpose(const Matrix<kind>& source)
 template<class kind>
 std::vector<kind>& operator *(const Matrix<kind>& A,const std::vector<kind>& b)
 {
-#ifdef DEBUG
-  assert(A.ncolumn == b.size());
-#endif
+  if (A.ncolumn != b.size()) throw std::invalid_argument("The matrix and vector dimensions don't conform for multiplication!");
   unsigned int i,j;
   kind sum;
   std::vector<kind> output;
@@ -892,9 +923,7 @@ std::vector<kind>& operator *(const Matrix<kind>& A,const std::vector<kind>& b)
 template<class kind>
 Matrix<kind>& operator +(const Matrix<kind>& A,const Matrix<kind>& B)
 {
-#ifdef DEBUG
-  assert(A.nrow == B.nrow && A.ncolumn == B.ncolumn);
-#endif
+  if (A.nrow != B.nrow || A.ncolumn != B.ncolumn) throw std::invalid_argument("The dimensions of the two matrices don't conform for addition!");
   unsigned int i,j,k,in1,cvalue;
   bool found;
   Matrix<kind> output(A.nrow,A.ncolumn);
@@ -927,9 +956,7 @@ Matrix<kind>& operator +(const Matrix<kind>& A,const Matrix<kind>& B)
 template<class kind>
 Matrix<kind>& operator *(const Matrix<kind>& A,const Matrix<kind>& B)
 {
-#ifdef DEBUG
-  assert(A.ncolumn == B.nrow);
-#endif
+  if (A.ncolumn != B.nrow) throw std::invalid_argument("The dimensions of the two matrices don't conform for multiplication!");
   unsigned int i,j,k,l,in1;
   kind sum;
   Matrix<kind> output(A.nrow,B.ncolumn);
@@ -1110,9 +1137,9 @@ namespace SYNARMOSMA {
   {
     // If (type == c), then column(n) -> -1*column(n)
     // If (type == r), then row(n) -> -1*row(n)
-    if (n >= A.nrow) throw std::invalid_argument("The row number argument is illegal for this matrix!");
     if (type != 'c' && type != 'r') throw std::invalid_argument("The matrix inversion type must be row-based or column-based!");
-    assert(n >= 0);
+    if (type == 'r' && n >= A.nrow) throw std::invalid_argument("The row number argument is illegal for this matrix inversion!");
+    if (type == 'c' && n >= A.ncolumn) throw std::invalid_argument("The column number argument is illegal for this matrix inversion!");
     unsigned int i,j;
 
     if (type == 'c') {
@@ -1134,10 +1161,10 @@ namespace SYNARMOSMA {
   {
     // If (type == r), then row(n) -> row(n) + q*row(m)
     // If (type == c), then column(n) -> column(n) + q*column(m)
-    if (n >= A.nrow) throw std::invalid_argument("The row number argument is illegal for this matrix!");
-    if (m >= A.ncolumn) throw std::invalid_argument("The column number argument is illegal for this matrix!");
     if (type != 'c' && type != 'r') throw std::invalid_argument("The matrix combination type must be row-based or column-based!");
-    unsigned int i,j,l = 0,nu = n,mu = m;
+    if (type == 'r' && (n >= A.nrow || m >= A.nrow)) throw std::invalid_argument("The row number argument is illegal for this matrix!");
+    if (type == 'c' && (n >= A.ncolumn || m >= A.ncolumn)) throw std::invalid_argument("The column number argument is illegal for this matrix!");
+    unsigned int i,j,l = 0;
     bool found,fd1,fd2;
     kind wv = Matrix<kind>::zero;
 
@@ -1146,12 +1173,12 @@ namespace SYNARMOSMA {
         fd1 = false;
         fd2 = false;
         for(j=0; j<A.elements[i].size(); ++j) {
-          if (A.elements[i][j].second == nu) {
+          if (A.elements[i][j].second == n) {
             fd1 = true;
             wv = q*A.elements[i][j].first;
             if (fd2) break;
           }
-          else if (A.elements[i][j].second == mu) {
+          else if (A.elements[i][j].second == m) {
             fd2 = true;
             l = j;
             if (fd1) break;
@@ -1195,14 +1222,14 @@ namespace SYNARMOSMA {
   template<class kind>
   void pivot_row(Matrix<kind>& A,unsigned int k,unsigned int l)
   {
-    if (k >= A.nrow) throw std::invalid_argument("The row number argument is illegal for this matrix!");
-    if (l >= A.ncolumn) throw std::invalid_argument("The column number argument is illegal for this matrix!");
-    unsigned int i,j,lu = l;
+    if (k >= A.nrow) throw std::invalid_argument("The row number argument is illegal for this matrix pivot!");
+    if (l >= A.ncolumn) throw std::invalid_argument("The column number argument is illegal for this matrix pivot!");
+    unsigned int i,j;
     bool found;
     kind n,q,d = Matrix<kind>::zero;
 
     for(i=0; i<A.elements[k].size(); ++i) {
-      if (A.elements[k][i].second == lu) {
+      if (A.elements[k][i].second == l) {
         d = A.elements[k][i].first;
         break;
       }
@@ -1210,7 +1237,7 @@ namespace SYNARMOSMA {
     for(i=k+1; i<A.nrow; ++i) {
       found = false;
       for(j=0; j<A.elements[i].size(); ++j) {
-        if (A.elements[i][j].second == lu) {
+        if (A.elements[i][j].second == l) {
           n = A.elements[i][j].first;
           found = true;
           break;
@@ -1227,12 +1254,12 @@ namespace SYNARMOSMA {
   {
     if (k >= A.nrow) throw std::invalid_argument("The row number argument is illegal for this matrix!");
     if (l >= A.ncolumn) throw std::invalid_argument("The column number argument is illegal for this matrix!");
-    unsigned int i,j,lu = l;
+    unsigned int i,j;
     bool found;
     kind n,q,d = Matrix<kind>::zero;
  
     for(i=0; i<A.elements[k].size(); ++i) {
-      if (A.elements[k][i].second == lu) {
+      if (A.elements[k][i].second == l) {
         d = A.elements[k][i].first;
         break;
       }
@@ -1256,7 +1283,7 @@ namespace SYNARMOSMA {
   void move_minimum(Matrix<kind>& A,unsigned int n)
   {
     if (n >= A.nrow) throw std::invalid_argument("The row number argument is illegal for this matrix!");
-    unsigned int i,j,im,jm,nu = n,istart = 0,ulimit = A.nrow - n;  
+    unsigned int i,j,im,jm,istart = 0,ulimit = A.nrow - n;  
     kind beta,alpha = Matrix<kind>::zero;
     bool full[ulimit];
     std::pair<kind,unsigned int>* VY = new std::pair<kind,unsigned int>[ulimit];
@@ -1265,7 +1292,7 @@ namespace SYNARMOSMA {
     for(i=n; i<A.nrow; ++i) {
       full[i-n] = false;
       for(j=0; j<A.elements[i].size(); ++j) {
-        if (A.elements[i][j].second >= nu) {
+        if (A.elements[i][j].second >= n) {
           VX.push_back(A.elements[i][j]);
         }
       }
@@ -1320,7 +1347,7 @@ namespace SYNARMOSMA {
   void prepare_matrix(Matrix<kind>& A,unsigned int n)
   {
     if (n >= A.nrow) throw std::invalid_argument("The row number argument is illegal for this matrix!");
-    unsigned int i,j,v1,v2,nu = n;
+    unsigned int i,j,v1,v2;
     bool jump;
     kind f;
 
@@ -1330,7 +1357,7 @@ namespace SYNARMOSMA {
       jump = false;
       for(i=n+1; i<A.nrow; ++i) {
         for(j=0; j<A.elements[i].size(); ++j) {
-          if (A.elements[i][j].second == nu) {
+          if (A.elements[i][j].second == n) {
             jump = true;
             break;
           }
@@ -1341,7 +1368,7 @@ namespace SYNARMOSMA {
       pivot_column(A,n,n);
       jump = false;
       for(i=0; i<A.elements[n].size(); ++i) {
-        if (A.elements[n][i].second > nu) {
+        if (A.elements[n][i].second > n) {
           jump = true;
           break;
         }
