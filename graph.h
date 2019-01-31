31aa0bc4bf2261c203bea97578112ba61e60dd76
Daniel Stubbs <dnl.stubbs@gmail.com>
2019-01-31 07:45:34 -0500

    I continued to work on documenting the Graph class, which is now perhaps half-way done, and converted a few smaller methods to being inline.

--- graph.h
+++ graph.h
@@ -60,27 +60,38 @@ namespace SYNARMOSMA {
     // Hyphantic operators
     bool stellar_addition(int);
     bool stellar_deletion(int);
+    /// This method fuses together the two vertices that are its arguments; if the final argument is -1, it chooses a neighbouring vertex of the first argument at random to fuse with this first argument. In this fusion the edge sets of the two vertices are combined together and the method returns true if the fusion is successful.
     virtual bool fusion(int,int);
+    /// This method eliminates the edge joining the two vertices that are its arguments; if the final argument is -1, it chooses a neighbouring vertex of the first argument at random to eliminate, returning true if successful.
     virtual bool foliation_x(int,int);
+    /// This method adds an edge joining the two vertices that are its arguments; if the final argument is -1, it chooses another distinct vertex at random and adds an edge between them, returning true if this is a new edge.
     virtual bool foliation_m(int,int);
+    /// This method adds a new vertex to the graph and an edge joining this new vertex to the vertex that is the argument of the method, returning the index of the newly created vertex.
     virtual int fission_x(int);
+    /// This method adds a new vertex to the graph along with edges joining this new vertex to the vertex that is the argument of this method and all its neighbours, while returning the index of the newly created vertex. 
     virtual int fission_m(int);
     /// This method computes the maximum network flow from a source vertex (first argument) to a sink vertex (second argument), returning the value of the flow.
     virtual double compute_flow(int,int);
     void core(Graph*,int) const;
     int eccentricity(int) const;
+    /// This method returns true if the graph is planar, false otherwise. 
     bool planar() const;
     bool biconnected() const;
     double cosine_similarity(int,int) const;
+    /// This method computes the length of the graph's largest cycle; it returns -1 if the graph is acyclic. 
     int girth() const;
-    double inverse_girth() const;
+    /// This method computes the inverse girth; if the graph is acyclic it returns zero.
+    inline double inverse_girth() const;
+    /// This method computes the complement of this graph, i.e. the graph with the same vertices but where two vertices are connected if and only if they are not connected in the original graph.
     void complement(Graph*) const;
     void tutte_polynomial(std::vector<Monomial<int> >&) const;
     double clustering_coefficient(int) const;
     double clustering_coefficient() const;
     double mean_path_length() const;
+    /// This method computes the number of edges which are bridges, i.e. edges which if deleted would disconnect the graph.
     int bridge_count() const;
     bool bipartite() const;
+    /// This method computes the graph's cyclicity, i.e. the percentage of edges whose deletion would not disconnect the graph.
     double cyclicity() const;
     double connectivity() const;
     int omega() const;
@@ -90,15 +101,22 @@ namespace SYNARMOSMA {
     double percolation(bool) const;
     double cyclic_resistance() const;
     double entwinement() const;
-    double completeness() const;
-    int circuit_rank() const;
+    /// This method computes the graph's completeness, i.e. the graph's size divided by \f$N(N-1)/2\f$, where \f$N\f$ is the graph's order; this measures how closely it approximates the complete graph on \f$N\f$ vertices.
+    inline double completeness() const;
+    /// This method computes the graph's circuit rank, defined to be the number of graph components minus the number of vertices plus the number of edges.
+    inline int circuit_rank() const;
+    /// This method computes the graph's chromatic number, i.e. the smallest number of colours which can be used to colour the vertices such that no two neighbouring vertices share the same colour.
     int chromatic_number() const;
     int boundary_nodes() const;
-    int max_degree() const;
-    int min_degree() const;
-    double average_degree() const;
+    /// This method returns the minimum degree of the graph.
+    inline int max_degree() const;
+    /// This method returns the maximum degree of the graph.
+    inline int min_degree() const;
+    /// This method returns the arithmetic mean of the vertex degrees of the graph.
+    inline double average_degree() const;
     double return_probability(int,int) const;
     void random_walk(double*,double*,int) const;
+    /// This method computes the adjacency matrix of the graph and stores it in an instance of the Binary_Matrix class, which is the method's unique argument.
     void compute_adjacency_matrix(Binary_Matrix*) const;
     int compute_deformed_laplacian(std::complex<double>,Matrix<std::complex<double> >*) const;
     int compute_laplacian(Matrix<double>*) const;
@@ -114,5 +132,62 @@ namespace SYNARMOSMA {
     /// This method writes the graph out to a disk file in the DOT format for visualization by GraphViz; the method's argument is the filename.
     virtual void write2disk(const std::string&) const;
   };
+
+  double Graph::inverse_girth() const
+  {
+    int output = girth();
+    double g = 0.0;
+    if (output > 0) g = 1.0/double(output);
+  
+    return g;
+  }
+
+  int Graph::circuit_rank() const
+  {
+    int chi = size() - nvertex;
+    if (connected()) return 1 + chi;
+    std::vector<int> components;
+    int n = component_analysis(components);
+    return n + chi;
+  }
+
+  int Graph::max_degree() const
+  {
+    int i,n,output = 0;
+    for(i=0; i<nvertex; ++i) {
+      n = (signed) neighbours[i].size();
+      if (n > output) output = n;
+    }
+    return output;
+  }
+
+  int Graph::min_degree() const
+  {
+    int i,n,output = 1 + size();
+    for(i=0; i<nvertex; ++i) {
+      n = (signed) neighbours[i].size();
+      if (n < output) output = n;
+    }
+    return output;
+  }
+
+  double Graph::average_degree() const
+  {
+    int i;
+    double sum = 0.0;
+    for(i=0; i<nvertex; ++i) {
+      sum += double(neighbours[i].size());
+    }
+    return sum/double(nvertex);
+  }
+
+  double Graph::completeness() const
+  {
+    double output = 0.0;
+    if (nvertex == 1) return output;
+
+    output = 2.0*double(size())/double(nvertex*(nvertex-1));
+    return output;
+  }
 }
 #endif
